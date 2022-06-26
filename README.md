23fd93520d1983718a8e5e00ac750cc9f2c43af3
hamza yerrou <hamzayerrou5@yahoo.com>
2022-06-26 01:18:52 +0100

    .

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ehya
+
+/_ open terminal and type _/
+gulp
+
+/_ copy the local host and paste it into any browser to start a live project _/
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var StaticServer = require('static-server');
 
 
 var server = new StaticServer({
-  rootPath: './build', // required, the root of the server file tree
+  rootPath: './dist', // required, the root of the server file tree
   port: 1337, // required, the port to listen
 });
 
